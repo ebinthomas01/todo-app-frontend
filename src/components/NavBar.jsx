a4f4f5e09b680d881f82dc521f41c06e9b0686e3
ebinthomas01 <ebintkm@gmail.com>
2024-06-12 23:30:35 +0530

    navbar added

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand " href="/"> CREATE NEW ACCOUNT</a>
+    
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default NavBar
