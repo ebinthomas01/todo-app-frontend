1344d557590bf6fe0f971bfb44e0908cf99db53b
ebinthomas01 <ebintkm@gmail.com>
2024-06-12 22:54:25 +0530

    adduser page finised

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/login';
+import AddUser from './components/AddUser';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login/>}/>
+        <Route path='/AddUser' element={<AddUser/>}/>
       </Routes>
     </BrowserRouter>
   );
